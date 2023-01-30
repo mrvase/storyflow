1acc711ff95ef5f46a83843cd69aa76e117dea34
mrvase <martin@rvase.dk>
2023-01-30 19:25:32 +0100

    Add rollup build config for frontend

---
+++ packages/frontend/rollup.config.ts
@@ -0,0 +1,30 @@
+import dts from "rollup-plugin-dts";
+import esbuild from "rollup-plugin-esbuild";
+import { defineConfig } from "rollup";
+
+export default defineConfig([
+  {
+    plugins: [esbuild({ minify: true })],
+    output: [
+      {
+        file: `dist/index.js`,
+        format: "cjs",
+      },
+      {
+        file: `dist/index.mjs`,
+        format: "es",
+      },
+    ],
+    input: "events.ts",
+    external: (id) => !/^[./]/.test(id),
+  },
+  {
+    plugins: [dts()],
+    output: {
+      file: `dist/index.d.ts`,
+      format: "es",
+    },
+    input: "events.ts",
+    external: (id) => !/^[./]/.test(id),
+  },
+]);
