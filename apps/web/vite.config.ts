a0e345ae1a02931b7364ae66bf02a13104786a6c
mrvase <martin@rvase.dk>
2023-01-31 12:24:54 +0100

    Prevent middleware from catching dashboard assets

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "public",
-    assetsDir: "dashboard",
+    assetsDir: "dashboard/assets",
     emptyOutDir: false,
   },
 });
