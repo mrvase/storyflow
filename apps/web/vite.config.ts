c45a854bd9c3fe1dace6c76e45b3dff84e515a43
mrvase <martin@rvase.dk>
2023-01-31 12:17:36 +0100

    Change build to allow other slugs in vite dev

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -4,8 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dashboard",
   build: {
-    outDir: "public/dashboard",
+    outDir: "public",
+    assetsDir: "dashboard",
+    emptyOutDir: false,
   },
 });
