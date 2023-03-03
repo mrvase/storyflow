c66237a853d49785dd3d666f9a4ff92de3a75ba7
mrvase <martin@rvase.dk>
2023-03-03 13:54:33 +0100

    try again 9

--- apps/web/src/app/request.ts
+++ apps/web/src/app/request.ts
@@ -4,7 +4,9 @@ import { config } from "../components";
 const IS_DEV = process.env.NODE_ENV === "development";
 
 const apiKey = Buffer.from(process.env.API_KEY as string).toString("base64");
-const domain = IS_DEV ? "http://localhost:3000" : "https://www.storyflow.dk";
+const domain = IS_DEV
+  ? "http://localhost:3000"
+  : "https://storyflow-mrvase.vercel.app";
 
 export const request = async (url: string) => {
   const fetchUrl = `${domain}/api/public/get?query[namespace]=${
@@ -13,8 +15,6 @@ export const request = async (url: string) => {
   console.log("FETCHING", fetchUrl);
   try {
     const res = await fetch(fetchUrl, {
-      method: "GET",
-      cache: "force-cache",
       headers: new Headers({
         "x-storyflow": apiKey,
       }),
@@ -37,12 +37,11 @@ export const request = async (url: string) => {
 };
 
 export const requestPaths = async () => {
+  console.log("REQUESTING PATHS");
   try {
     const res = await fetch(
       `${domain}/api/public/getPaths?query=${process.env.NAMESPACE ?? ""}`,
       {
-        method: "GET",
-        cache: "force-cache",
         headers: new Headers({
           "x-storyflow": apiKey,
         }),
